891dd912fa887f3a277287f09d5a8411eed212e0
Aidan Richards <17afer@queensu.ca>
2022-05-27 10:06:09 -0400

    Initial Commit

---
+++ SelfCheckout/SelfCheckoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfCheckoutApp.swift
+//  SelfCheckout
+//
+//  Created by Aidan Richards on 2022-05-27.
+//
+
+import SwiftUI
+
+@main
+struct SelfCheckoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
